ce55e1165ce1b2d2033df0ec01104574435d35b7
JoseAcevedo <acevedo0803@gmail.com>
2025-04-14 21:10:17 -0300

    run querys and get openai response ok

--- app_bot_hibrido/admin.py
+++ app_bot_hibrido/admin.py
@@ -4,22 +4,35 @@ from .models import *
 
 
 @admin.register(Client)
-class QuoteCompanyQuoteAdmin(admin.ModelAdmin):
+class ClientAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Client._meta.fields]
     pass
 
+
 @admin.register(Context)
-class QuoteCompanyQuoteAdmin(admin.ModelAdmin):
+class ContextAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(Course)
-class QuoteCompanyQuoteAdmin(admin.ModelAdmin):
+class CourseAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Course._meta.fields]
     list_filter = ['is_active']
     pass
 
+
+@admin.register(DatabaseDriver)
+class DatabaseDriverAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ExternalDatabase)
+class ExternalDatabaseAdmin(admin.ModelAdmin):
+    pass
+
+
 @admin.register(User)
-class QuoteCompanyQuoteAdmin(admin.ModelAdmin):
+class UserAdmin(admin.ModelAdmin):
     list_display = [field.name for field in User._meta.fields]
     list_filter = ['is_teacher']
     pass
